876a195946e7081ad642d9f46b89c11c37e1a519
ajburnell <30587915+ajburnell@users.noreply.github.com>
2023-07-23 14:50:52 +1000

    Add route 53

--- ctfd/variables.tf
+++ ctfd/variables.tf
@@ -7,3 +7,8 @@ variable "private_key_filename" {
   type = string
   description = "The filename to write the private key too."
 }
+
+variable "r53_zone_id" {
+  type = string  
+  description = "The Zone ID for where we want to create the Route 53 records."
+}
