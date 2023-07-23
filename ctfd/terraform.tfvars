aecd302f312d77b0e46e89b1133b48bcfcb19fa0
ajburnell <30587915+ajburnell@users.noreply.github.com>
2023-07-23 12:33:46 +1000

    Update and rename terraform.tfvars to variables.tf

--- ctfd/terraform.tfvars
+++
@@ -1,9 +0,0 @@
-variable "private_key_filename" {
-  type = string
-  default = "private_key.pem"
-}
-
-variable "public_key_filename" {
-  type = string
-  default = "public_key.pub"
-}
---
+++ ctfd/variables.tf
@@ -0,0 +1,9 @@
+variable "public_key_filename" {
+  type = string
+  description = "The filaname to write the public key too."
+}
+
+variable "private_key_filename" {
+  type = string
+  description = "The filename to write the private key too."
+}
